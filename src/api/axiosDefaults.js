6d7a45bcf33cec28df233a0c255f0c8709950df5
Zahra Hassani <raha.zahra1375@gmail.com>
2023-05-11 07:14:37 +0000

    add access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://8000-zahraraha-drfrepo-fgqhxi88bs3.ws-us97.gitpod.io'
 axios.defaults.headers.post['Content-Type'] = 'maltipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
